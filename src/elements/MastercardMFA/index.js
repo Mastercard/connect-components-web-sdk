113dd021260842e5565a562bee0a31558244c83a
srilekha allamsetty <srilekha.allamsetty@mastercard.com>
2023-07-13 11:24:37 -0400

    removed other files

--- src/elements/MastercardMFA/index.js
+++
@@ -1,3 +0,0 @@
-import mastercardMfa_injector from './MastercardMFA.element';
-
-export default mastercardMfa_injector();
--- src/index.js
+++ src/index.js
@@ -3,10 +3,8 @@ import MastercardForm from './elements/MastercardForm';
 import MastercardInput from './elements/MastercardInput';
 import MastercardSubmit from './elements/MastercardSubmit';
 import MastercardMFAChoice from './elements/MastercardMFAChoice';
-import MastercardMFA from './elements/MastercardMFA';
 
 customElements.define('mastercard-form', MastercardForm);
 customElements.define('mastercard-input', MastercardInput);
 customElements.define('mastercard-submit', MastercardSubmit);
 customElements.define('mastercard-mfa-choice', MastercardMFAChoice);
-customElements.define('mastercard-mfa', MastercardMFA);
