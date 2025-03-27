a27117db4d3e75766567f330d27b24af07c18ff1
Scott Peterson <scott.peterson@mastercard.com>
2025-03-27 17:04:19 -0600

    build: multi build types

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,12 +1,38 @@
-const { merge } = require('webpack-merge');
-const common = require('./webpack.config.common.js');
+// const { merge } = require('webpack-merge');
+// const common = require('./webpack.config.common.js');
 const TerserPlugin = require('terser-webpack-plugin');
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
 
-module.exports = merge(common, {
+const moduleFormat = {
+  mode: 'production',
+  devtool: 'source-map',
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        minify: TerserPlugin.uglifyJsMinify,
+      }),
+    ],
+  },
+  entry: './src/index.js',
+  plugins: [new Dotenv()],
+  experiments: {
+    outputModule: true,
+  },
+  output: {
+    filename: 'sdk.mjs',
+    path: path.resolve(__dirname, 'dist'),
+    clean: false,
+    libraryTarget: 'module',
+  },
+};
+
+const commonJSFormat = {
   mode: 'production',
   devtool: 'source-map',
   output: {
-    filename: 'sdk.js',
+    filename: 'sdk.mjs',
   },
   optimization: {
     minimize: true,
@@ -16,4 +42,14 @@ module.exports = merge(common, {
       }),
     ],
   },
-});
+  entry: './src/index.js',
+  plugins: [new Dotenv()],
+  output: {
+    filename: 'sdk.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: false,
+    libraryTarget: 'commonjs2',
+  },
+};
+
+module.exports = [moduleFormat, commonJSFormat];
